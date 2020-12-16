b115f1847833e4cc3628a28eaf885d6237a9be86
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-16 22:20:24 +0100

    Complete the Features & Starting Footer

--- src/Pages/Home/FeaturedHome/Featured.jsx
+++ src/Pages/Home/FeaturedHome/Featured.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
+import Button from '../../../components/Button/Button'
+import './Featured.scss'
+import Feature1 from '../../../static/portfolio/desktop/image-del-sol.jpg'
+import Feature1Tab from '../../../static/portfolio/tablet/image-del-sol.jpg'
+import Feature1Mob from '../../../static/portfolio/mobile/image-del-sol.jpg'
+// import Feature2 from '../../../static/portfolio/desktop/image-228b.jpg'
+// import Feature3 from '../../../static/portfolio/desktop/image-prototype.jpg'
+import { Link } from 'react-router-dom'
+import FeaturedData from './FeaturedData'
 
 function Featured() {
     return (
         <div className="featured">
             <div className="container">
-                <div className="featured_header">
-                    <div className="featured_title">
+                <div className="featured_container">
+                    <div className="featured_header">
                         <h2>Featured</h2>
                         <Button>
                             See All
                         </Button>
+                    </div>
+                    <div className="featured_boxes">
+                        <Link to="/portfolio">
+                            {FeaturedData.map((item, index) => (
+                                <div className="featured_box" key={index}>
+                                    <div className="featured_box--img">
+                                        <picture>
+                                            <source srcSet={`${item.imgMob}`} media="(max-width: 767px)" />
+                                            <source srcSet={`${item.imgTab}`} media="(max-width: 1199px)" />
+                                            <source srcSet={`${item.img}`} media="(min-width: 1200px)" />
+                                            <img src={item.img} alt="featured-1" />
+                                        </picture>
+                                    </div>
+                                    <div className="featured_box--content">
+                                        <h3>{item.title}</h3>
+                                        <p>View All Projects</p>
+                                    </div>
+                                    <div className="featured_box--number">
+                                        <h2>{item.num}</h2>
+                                    </div>
+                                </div>
+                            ))}
+                        </Link>
                     </div>
                 </div>
             </div>
---
+++ src/Pages/Home/FeaturedHome/FeaturedData.js
@@ -0,0 +1,37 @@
+import Feature1 from '../../../static/portfolio/desktop/image-del-sol.jpg'
+import Feature1Tab from '../../../static/portfolio/tablet/image-del-sol.jpg'
+import Feature1Mob from '../../../static/portfolio/mobile/image-del-sol.jpg'
+
+import Tower1 from '../../../static/portfolio/desktop/image-228b.jpg'
+import Tower1Tab from '../../../static/portfolio/tablet/image-228b.jpg'
+import Tower1Mob from '../../../static/portfolio/mobile/image-228b.jpg'
+
+import Prototype1 from '../../../static/portfolio/desktop/image-prototype.jpg'
+import Prototype1Tab from '../../../static/portfolio/tablet/image-prototype.jpg'
+import Prototype1Mob from '../../../static/portfolio/mobile/image-prototype.jpg'
+
+const FeaturedData = [
+    {
+        img: Feature1,
+        imgTab: Feature1Tab,
+        imgMob: Feature1Mob,
+        title: 'Project Del Sol',
+        num: '1',
+    },
+    {
+        img: Tower1,
+        imgTab: Tower1Tab,
+        imgMob: Tower1Mob,
+        title: '288B Tower',
+        num: '2',
+    },
+    {
+        img: Prototype1,
+        imgTab: Prototype1Tab,
+        imgMob: Prototype1Mob,
+        title: 'Le Prototype',
+        num: '3',
+    },
+]
+
+export default FeaturedData
