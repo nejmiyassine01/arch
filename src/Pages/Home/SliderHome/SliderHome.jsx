9ef649b1f827a3d0032590366ef57f8896a2d743
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-18 00:00:31 +0100

    Update The Fixed Component and the packages

--- src/Pages/Home/SliderHome/SliderHome.jsx
+++ src/Pages/Home/SliderHome/SliderHome.jsx
@@ -15,34 +15,32 @@ function SliderHome() {
     }
     
     return (
-        <>
-            <div className="slider">
-                <div className="container" key={slides.id}>
-                    <div className="slider_image">
-                        <picture>
-                            <source srcSet={`${slides.imgMob}`} media="(max-width: 800px)" />
-                            <source srcSet={`${slides.imgTab}`} media="(max-width: 1180px)" />
-                            <source srcSet={`${slides.img}`} media="(min-width: 1181px)" />
-                            <img src={slides.img} alt="Paramor" />
-                        </picture>
-                    </div>
-                    <div className="slider_content">
-                        <h2>{slides.title}</h2>
-                        <p>{slides.text}</p>
-                        <Button to="/portfolio">
-                            See Our Portfolio
-                        </Button>
-                    </div>
+        <div className="slider">
+            <div className="container" key={slides.id}>
+                <div className="slider_image">
+                    <picture>
+                        <source srcSet={`${slides.imgMob}`} media="(max-width: 800px)" />
+                        <source srcSet={`${slides.imgTab}`} media="(max-width: 1180px)" />
+                        <source srcSet={`${slides.img}`} media="(min-width: 1181px)" />
+                        <img src={slides.img} alt="Paramor" />
+                    </picture>
                 </div>
-                
-                <div className="slider_numbers">
-                    <div className="slider_number" id="0" onClick={onSlide}>01</div>
-                    <div className="slider_number" id="1" onClick={onSlide}>02</div>
-                    <div className="slider_number" id="2" onClick={onSlide}>03</div>
-                    <div className="slider_number" id="3" onClick={onSlide}>04</div>
+                <div className="slider_content">
+                    <h2>{slides.title}</h2>
+                    <p>{slides.text}</p>
+                    <Button to="/portfolio">
+                        See Our Portfolio
+                    </Button>
                 </div>
             </div>
-        </>
+            
+            <div className="slider_numbers">
+                <div className="slider_number" id="0" onClick={onSlide}>01</div>
+                <div className="slider_number" id="1" onClick={onSlide}>02</div>
+                <div className="slider_number" id="2" onClick={onSlide}>03</div>
+                <div className="slider_number" id="3" onClick={onSlide}>04</div>
+            </div>
+        </div>
     )
 }
 
