6031299b6a6890c6066de64828668e9d04206d0f
nejmiyassine01 <nejmi.yassine01@gmail.com>
2020-12-17 22:01:01 +0100

    Finished The Project

--- src/Pages/Contact/MapApi/MapApi.jsx
+++ src/Pages/Contact/MapApi/MapApi.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
 
-function MapApi() {
-    return (
-        <div>
-            <h2>Map</h2>
-        </div>
-    )
+
+const Office1 = ({ text }) => <div>{text}</div>;
+
+class MapApi extends React.Component {
+    
+    static defaultProps = {
+        center: {
+          lat: 59.95,
+          lng: 30.33
+        },
+        zoom: 11
+    };
+
+    render() {
+        return (
+          // Important! Always set the container height explicitly
+          <div style={{ height: '600px', width: '90%', margin: '0 auto' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: 'AIzaSyB13_qAHHWC1DVy3vhHA9Kk1Uygnf1Fx2Q' }}
+              defaultCenter={this.props.center}
+              defaultZoom={this.props.zoom}
+            >
+              <Office1
+                lat={59.955413}
+                lng={30.337844}
+                text="Office"
+              />
+            </GoogleMapReact>
+          </div>
+        );
+    }
 }
 
 export default MapApi
