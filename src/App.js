9b2b186e1cbce541347ca81da3b022c8dc0703ec
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-01-21 22:22:38 +0100

    Final Code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import './App.css';
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Home from './Pages/Home/Home'
-import Portfolio from './Pages/Portfolio/Portfolio'
-import About from './Pages/About/About'
-import Contact from './Pages/Contact/Contact'
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/about" component={About} />
-        </Switch>
-        <Footer/>
-      </Router>  
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/portfolio" component={Portfolio} />
+					<Route path="/contact" component={Contact} />
+					<Route path="/about" component={About} />
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/Pages/Home/SliderHome/SliderHome.jsx
+++ src/Pages/Home/SliderHome/SliderHome.jsx
@@ -1,47 +1,74 @@
-import React, { useState } from 'react'
-import './SliderHome.scss'
-import SlideHomeData from './SlideHomeData'
-import Button from '../../../components/Button/Button'
+import React, { useState, useRef, useEffect } from "react";
+import "./SliderHome.scss";
+import SlideHomeData from "./SlideHomeData";
+import Button from "../../../components/Button/Button";
 
 function SliderHome() {
-    const [current, setcurrent] = useState(0);
-    const slides = SlideHomeData[current];
+	const [current, setcurrent] = useState(0);
+	const slides = SlideHomeData[current];
+	const length = SlideHomeData.length;
+	const timeout = useRef(null);
 
-    // if (!Array.isArray(slides) || slides.length <= 0) { return null }
-    
-    const onSlide = (e) => {
-        e.preventDefault()
-        setcurrent(parseInt(e.target.id))
-    }
-    
-    return (
-        <div className="slider">
-            <div className="container" key={slides.id}>
-                <div className="slider_image">
-                    <picture>
-                        <source srcSet={`${slides.imgMob}`} media="(max-width: 800px)" />
-                        <source srcSet={`${slides.imgTab}`} media="(max-width: 1180px)" />
-                        <source srcSet={`${slides.img}`} media="(min-width: 1181px)" />
-                        <img src={slides.img} alt="Paramor" />
-                    </picture>
-                </div>
-                <div className="slider_content">
-                    <h2>{slides.title}</h2>
-                    <p>{slides.text}</p>
-                    <Button to="/portfolio">
-                        See Our Portfolio
-                    </Button>
-                </div>
-            </div>
-            
-            <div className="slider_numbers">
-                <div className="slider_number" id="0" onClick={onSlide}>01</div>
-                <div className="slider_number" id="1" onClick={onSlide}>02</div>
-                <div className="slider_number" id="2" onClick={onSlide}>03</div>
-                <div className="slider_number" id="3" onClick={onSlide}>04</div>
-            </div>
-        </div>
-    )
+	const onSlide = (e) => {
+		setcurrent(parseInt(e.target.id));
+
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+		}
+	};
+
+	useEffect(() => {
+		const onSlide = () => {
+			setcurrent((current) => (current === length - 1 ? 0 : current + 1));
+		};
+
+		timeout.current = setTimeout(onSlide, 3000);
+
+		return () => {
+			if (timeout.current) {
+				clearTimeout(timeout.current);
+			}
+		};
+	}, [current, length]);
+
+	if (!Array.isArray(slides) || slides.length <= 0) {
+		return null;
+	}
+
+	return (
+		<div className="slider">
+			<div className="container" key={slides.id}>
+				<div className="slider_image">
+					<picture>
+						<source srcSet={`${slides.imgMob}`} media="(max-width: 800px)" />
+						<source srcSet={`${slides.imgTab}`} media="(max-width: 1180px)" />
+						<source srcSet={`${slides.img}`} media="(min-width: 1181px)" />
+						<img src={slides.img} alt="Paramor" />
+					</picture>
+				</div>
+				<div className="slider_content">
+					<h2>{slides.title}</h2>
+					<p>{slides.text}</p>
+					<Button to="/portfolio">See Our Portfolio</Button>
+				</div>
+			</div>
+
+			<div className="slider_numbers">
+				<div className="slider_number" id="0" onClick={onSlide}>
+					01
+				</div>
+				<div className="slider_number" id="1" onClick={onSlide}>
+					02
+				</div>
+				<div className="slider_number" id="2" onClick={onSlide}>
+					03
+				</div>
+				<div className="slider_number" id="3" onClick={onSlide}>
+					04
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default SliderHome
+export default SliderHome;
